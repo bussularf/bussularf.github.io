c443bad4c53114594ac9b498eea9cfbb3a3a5a41
Fernanda de Jesus <fernandabussular@gmail.com>
2022-05-17 02:22:18 +0000

    corrige button

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["a Fernanda", "desenvolvedora"];
+const textArray = ["a Fernanda"];
 const typingDelay = 200;
 const erasingDelay = 100;
 const newTextDelay = 2000; // Delay between current and next text
@@ -21,20 +21,20 @@ function type() {
   }
 }
 
-function erase() {
-  if (charIndex > 0) {
-    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
-    charIndex--;
-    setTimeout(erase, erasingDelay);
-  } 
-  else {
-    cursorSpan.classList.remove("typing");
-    textArrayIndex++;
-    if(textArrayIndex>=textArray.length) textArrayIndex=0;
-    setTimeout(type, typingDelay + 1100);
-  }
-}
+// function erase() {
+//   if (charIndex > 0) {
+//     if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+//     typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+//     charIndex--;
+//     setTimeout(erase, erasingDelay);
+//   } 
+//   else {
+//     cursorSpan.classList.remove("typing");
+//     textArrayIndex++;
+//     if(textArrayIndex>=textArray.length) textArrayIndex=0;
+//     setTimeout(type, typingDelay + 1100);
+//   }
+// }
 
 document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
   if(textArray.length) setTimeout(type, newTextDelay + 250);
