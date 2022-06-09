06b290cfda3c1b658ef749351a7b304552a27ccd
Fernanda de Jesus <fernandabussular@gmail.com>
2022-06-09 23:46:22 +0000

    add efects in nav barr links

--- javascript/app.js
+++ javascript/app.js
@@ -14,7 +14,10 @@ const images = document.querySelectorAll(".images img");
 const prev_btn = document.querySelector(".prev-btn");
 const next_btn = document.querySelector(".next-btn");
 
+const links = document.querySelectorAll(".nav-link");
+
 window.addEventListener("scroll", () =>  {
+  activeLink();
   if (!skillsPlayed) skillsCounter();
   if (!mlPlayed) mlCounter();
 });
@@ -164,3 +167,23 @@ const swiper = new Swiper('.swiper', {
     clickable: true, 
   },
 });
+
+// Change Active Nav Links 
+
+function activeLink() {
+  let sections = document.querySelectorAll("section[id]");
+  let passedSections = Array.from(sections)
+  .map((sct, i) => {
+    return { y: sct.getBoundingClientRect().top - header.offsetHeight,
+    id: i,
+    };
+  })
+  .filter(sct => sct.y <= 0);
+
+  let currSectionId = passedSections.at(-1).id;
+  
+  links.forEach(l => l.classList.remove("active"));
+  links[currSectionId].classList.add("active");
+}
+
+activeLink();
