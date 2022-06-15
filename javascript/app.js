967fed47612f32b2a8773ae3faa49cc84d28e938
Fernanda de Jesus <fernandabussular@gmail.com>
2022-06-15 01:58:00 +0000

    768px

--- javascript/app.js
+++ javascript/app.js
@@ -220,3 +220,10 @@ hamburger.addEventListener("click", () => {
   document.body.classList.toggle("open");
   document.body.classList.toggle("stopScrolling");
 });
+
+links.forEach((link) =>
+  link.addEventListener("click", () => {
+    document.body.classList.remove("open");
+    document.body.classList.remove("stopScrolling");
+  })
+);
